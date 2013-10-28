d20aebba07aea94c560d760f03f4407e539f0996
Matthew <matthew.wu328@gmail.com>
2013-10-28 22:37:18 +0800

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-before_filter :authenticate_user!, except: [:index]
-http_basic_authenticate_with name: "manage", password: "steviematt", except: [:index]
+before_filter :authenticate_user!, except: [:show]
+http_basic_authenticate_with name: "manage", password: "steviematt", except: [:show]
 
 
 def index
