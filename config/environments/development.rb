3b905496f7bc01a6cde44048ea1d79133e867fe7
Matthew <matthew.wu328@gmail.com>
2013-11-03 12:22:35 +0800

    token fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,10 @@ Dine::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  
+config.action_mailer.default_url_options = {
+  :host => '127.0.0.1',
+  :port => 3000
+}
 
 
 end
