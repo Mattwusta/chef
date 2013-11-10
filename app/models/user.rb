94f1dbeffadf39aeaeb621d86d1cd53f2bc13712
Matthew <matthew.wu328@gmail.com>
2013-11-10 13:50:41 +0800

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :confirmable, :rememberable, :trackable, :validatable
 
-
+ validates :image, presence: true
  validates :address, presence: true
  validates :phone, presence: true
-
+ has_attached_file :image, styles: { medium: "800x300>"}
+  validates_attachment :image, content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+  size: { less_than: 2.megabytes }
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :image, :image_file_name, :email, :password, :password_confirmation, :remember_me, :name, :address, :phone, :intro, :dish1, :dish2, :dish3
