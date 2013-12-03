8d2f817244176802730a8128b7b0075ae631fdad
Matthew <matthew.wu328@gmail.com>
2013-12-03 21:40:19 +0800

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :confirmable, :rememberable, :trackable, :validatable
 
-
+ has_attached_file :image, styles: { medium: "180x180>"}
  validates :address, presence: true
  validates :phone, presence: true
 
