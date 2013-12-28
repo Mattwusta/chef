07647506fcf88f7a6f9054e58d11e41e16c77508
Matthew <matthew.wu328@gmail.com>
2013-12-28 15:15:13 +0800

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-	http_basic_authenticate_with name: "manage", password: "steviematt", except: [:about, :side, :line, :help, :dogood, :lovemeal, :doogood, :hunger,:delivery, :donations, :_home, :faq, :account, :return, :dish1, :dish2, :dish3, :give, :how, :know, :list, :menu, :privacy, :search, :service, :shop, :why, :cancel]
+	http_basic_authenticate_with name: "manage", password: "steviematt", except: [:dish4, :dish5, dish6, :about, :side, :line, :help, :dogood, :lovemeal, :doogood, :hunger,:delivery, :donations, :_home, :faq, :account, :return, :dish1, :dish2, :dish3, :give, :how, :know, :list, :menu, :privacy, :search, :service, :shop, :why, :cancel]
 
 def home
 end
@@ -53,6 +53,15 @@ def dish2
 end
 
 def dish3
+end
+
+def dish4
+end
+
+def dish5
+end
+
+def dish6
 end
 
 def side
--- app/models/orderlist.rb
+++ app/models/orderlist.rb
@@ -1,5 +1,5 @@
 class Orderlist < ActiveRecord::Base
-  attr_accessible :description, :dish1, :gift, :re, :ship, :discount, :paylater, :detail, :reciever, :account, :wireamount, :dish2, :dish3, :dishsum, :time, :phone, :address
+  attr_accessible :description, :dish1, :gift, :re, :ship, :discount, :paylater, :detail, :reciever, :account, :wireamount, :dish2, :dish3, :dish4, :dish5, :dish6, :dishsum, :time, :phone, :address
  
 
   belongs_to :user
@@ -7,6 +7,9 @@ class Orderlist < ActiveRecord::Base
   validates :dish1, presence: true
   validates :dish2, presence: true
   validates :dish3, presence: true  
+  validates :dish4, presence: true
+  validates :dish5, presence: true
+  validates :dish6, presence: true  
   
 end 
 
